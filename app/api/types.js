c784c24bf795ec89c2a5fc64b551b69a35639008
Boris Olskiy <boris.olskiy@confirmit.com>
2021-03-26 13:13:44 +0100

    Replace content on redirect to latest version of documentation

--- app/api/types.js
+++
@@ -1,6 +0,0 @@
-/**
- * @typedef {Object} CultureInfo
- * @property {string} dateFormat Required format, e.g. "M/d/yyyy".
- * @property {string[]} Names of months in current respondent language.
- * @property {string} name Culture name, e.g. "en-US".
- */
--- app/views/questions/single-slider-question-view.js
+++
@@ -1,7 +0,0 @@
-import QuestionWithAnswersView from './base/question-with-answers-view.js'
-
-export default class SingleSliderQuestionView extends QuestionWithAnswersView {
-    constructor(question) {
-        super(question);
-    }
-}
